965c36212c386c2d22ee2583a08eddf46380b38e
markjessell <markjessell@users.noreply.github.com>
2023-07-07 16:43:31 +0800

    Add Display behaviour via WAXI QFIELD Plugin

--- README.md
+++ README.md
@@ -7,9 +7,10 @@
 ## Usage
  1- Download zip file from github, install into QGIS using plugin manager   
  2- Select a layer that has structural info, then select the points you want to plot   
- 3- Click on WAXI Stereonet icon    ![plugin_icon](icon.png)  
-
-## Roadmap:
--	Plot poles and/or great circles for planes (user choice?),   
--	Plot great circles and poles of planes on which lines lie when the information is available,   
+ 3- You can use the WAXI QFIELD Plugin to control display behaviour
+ 4- Click on WAXI Stereonet icon    ![plugin_icon](icon.png)  
+ 
+ ## Roadmap:
+-	Plot poles and/or great circles for planes (user choice?),  Done using WAXI QFIELD Plugin for control 
+-	Plot great circles and poles of planes on which lines lie when the information is available,   Done (I think?)
 -	Plot the hanging wall relative motion vector with an arrow on pole symbol if kinematic information is available.   
--- __init__.py
+++ __init__.py
@@ -32,6 +32,7 @@ from .mplstereonet import *
 from qgis.core import *
 from qgis.gui import *
 import os
+from qgis.core import QgsProject
 
 def classFactory(iface):
     return Stereonet(iface)
@@ -56,10 +57,24 @@ class Stereonet:
         dname='Dip'
         aname='Azimuth'
         pname='Plunge'
+        srefname='Strike_ref'
+        drefname='Dip_ref'
         strikes = list()
         dips = list()
- #       layers = self.iface.legendInterface().layers()
+        strikesref = list()
+        dipsref = list()
 
+        project = QgsProject.instance()
+        proj_file_path=project.fileName()
+        head_tail = os.path.split(proj_file_path)
+
+        gtCircles_flag_path = head_tail[0]+"/0. FIELD DATA/0. CURRENT MISSION/0. STOPS-SAMPLING-PHOTOGRAPHS-COMMENTS/gtCircles_flag.txt"
+
+        
+        if(os.path.exists(gtCircles_flag_path)):
+            gtCircles=True
+        else:
+            gtCircles=False
         
         self.iface.layerTreeView().selectedLayers()
 
@@ -74,12 +89,14 @@ class Stereonet:
                 dipExists = layer.fields().lookupField(dname)
                 azimuthExists = layer.fields().lookupField(aname)
                 plungeExists = layer.fields().lookupField(pname)
+                srefExists = layer.fields().lookupField(srefname)
+                drefExists = layer.fields().lookupField(drefname)
                 
                 for feature in iter:
                     if strikeExists != -1 and dipExists != -1:
                         strikes.append(feature[sname])
                         dips.append(feature[dname])
-
+                  
                     elif ddrExists != -1 and dipExists != -1:
                         strikes.append(feature[ddname]+90)
                         dips.append(feature[dname])
@@ -88,16 +105,31 @@ class Stereonet:
                         strikes.append(feature[aname]+90)
                         dips.append(90-feature[pname])
 
+                    if srefExists != -1 and drefExists != -1:
+                        if(not feature[srefname] is None and not feature[drefname] is None):
+                            strikesref.append(feature[srefname])
+                            dipsref.append(feature[drefname])
+
             else:
                 continue
+        strikesref = [i for i in strikesref if i != None]
+        dipsref = [i for i in dipsref if i != None]
+        strikes = [i for i in strikes if i != None]
+        dips = [i for i in dips if i != None]
+        #print(strikes)
 
-        if len(strikes) != 0:
+        if (len(strikes) != 0):
             fig, ax = mplstereonet.subplots()
             ax.set_azimuth_ticks([0,30,60,90,120,150,180,210,240,270,300,330])
             ax.set_azimuth_ticklabels(['0\u00b0','30\u00b0','60\u00b0','90\u00b0','120\u00b0','150\u00b0','180\u00b0','210\u00b0','240\u00b0','270\u00b0','300\u00b0','330\u00b0'])
             ax.grid(kind='equal_area_stereonet')
             ax.density_contour(strikes, dips, measurement='poles',cmap=cm.coolwarm,method='exponential_kamb',sigma=1.5,linewidths =0.5)
-            ax.pole(strikes, dips, 'k.', markersize=7)
+            if(gtCircles and strikeExists != -1):
+                ax.plane(strikes, dips, 'k',linewidth=1)
+            else:
+                ax.pole(strikes, dips, 'k.', markersize=5)
+                if(srefExists != -1 and drefExists != -1 ):
+                    ax.plane(strikesref,dipsref,'k',linewidth=1)
             ax.set_title(layer.name()+" [# "+str(len(iter))+"]")
             plt.show()
         else:
