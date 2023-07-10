d47ad16eae0868bc20dca74853f07e81e1f2a400
Mark Jessell <00073294@uwa.edu.au>
2023-07-10 19:53:09 +0800

    Add kinematics arrows for lineations

--- __init__.py
+++ __init__.py
@@ -33,6 +33,8 @@ from qgis.core import *
 from qgis.gui import *
 import os
 from qgis.core import QgsProject
+from math import asin,sin,degrees,radians,cos
+import json
 
 def classFactory(iface):
     return Stereonet(iface)
@@ -51,6 +53,81 @@ class Stereonet:
         self.iface.removeToolBarIcon(self.contourAction)
         del self.contourAction
 
+    def plungedip2rake(self,plunge,dip,kinematics):
+        if(plunge==dip):
+            rake=0
+        elif(dip==90.0):
+            rake=90-plunge
+        else:
+            rake=90-degrees(asin(sin(radians(plunge))/sin(radians(dip))))
+        
+        return(rake)
+
+    def waxi_fault_and_striae_plot(self,ax, pstrikes,pdips,strikes, dips, rakes,kinematics):
+        sos=['Sinistral-slip','Dextral-Slip','Normal-slip','Reverse-slip']
+        """Makes a fault-and-striae plot (a.k.a. "Ball of String") for normal faults
+        with the given strikes, dips, and rakes."""
+        # Plot the planes
+        #print(kinematics)
+        #print(strikes)
+        #print(dips)
+        #print(rakes)
+
+        # loop through each kinematic sense to plot correct orientations of arrows
+        for sos_it in sos:
+            s=list()
+            d=list()
+            r=list()            
+            dp=list()
+            sp=list()
+            for i in range(len(strikes)):
+                if(kinematics[i]==sos_it):
+                    s.append(pstrikes[i])
+                    d.append(pdips[i])
+                    r.append(rakes[i])
+                    dp.append(dips[i])
+                    sp.append(strikes[i])
+                    #print(i,strikes[i],dips[i],rakes[i])
+            # Calculate the position of the rake of the lineations, but don't plot yet
+            x,y = mplstereonet.rake(s, d,r)
+            x1,y1 = mplstereonet.pole(sp, dp)
+
+            # Calculate the direction the arrows should point
+            # These are all normal faults, so the arrows point away from the center
+            # For thrusts, it would just be u, v = -x/mag, -y/mag
+            mag = np.hypot(x, y)
+
+            theta = np.deg2rad(0)
+
+            rot = np.array([[cos(theta), -sin(theta)], [sin(theta), cos(theta)]])
+
+            v1 = np.array([x, y])
+
+            v2 = np.matmul(rot, v1)
+            u, v = v2[0] / mag, v2[1] / mag
+
+            # Plot the arrows at the rake locations...
+            #print(sos_it,s,d,r)
+            if(sos_it == sos[0] and len(x)>0):
+                #print("rot")
+                #print(v)
+                #print(v2)
+
+                arrows = ax.quiver(x1, y1, v, u, width=1, headwidth=4, units='dots',color='r')
+                #arrows = ax.quiver(x1, y1, -v, -u, width=1, headwidth=4, units='dots',color='b')
+                #arrows = ax.quiver(x1, y1, u, -v, width=1, headwidth=4, units='dots',color='g')
+                #arrows = ax.quiver(x1, y1, -u, v, width=1, headwidth=4, units='dots',color='k')
+            elif(sos_it == sos[1]and len(x)>0):
+                arrows = ax.quiver(x1, y1, -v, -u, width=1, headwidth=4, units='dots',color='g')
+            elif(sos_it == sos[2]and len(x)>0):
+                arrows = ax.quiver(x1, y1, -u, v, width=1, headwidth=4, units='dots',color='b')
+            elif(sos_it == sos[3]and len(x)>0):
+                #arrows = ax.quiver(x1, y1, v, u, width=1, headwidth=4, units='dots',color='r')
+                #arrows = ax.quiver(x1, y1, -v, -u, width=1, headwidth=4, units='dots',color='b')
+                arrows = ax.quiver(x1, y1, u, -v, width=1, headwidth=4, units='dots',color='k')
+                #arrows = ax.quiver(x1, y1, -u, v, width=1, headwidth=4, units='dots',color='k')
+
+
     def contourPlot(self):
         sname='Strike_RHR'
         ddname='Dip_Dir'
@@ -59,22 +136,32 @@ class Stereonet:
         pname='Plunge'
         srefname='Strike_ref'
         drefname='Dip_ref'
+        kname='Kinematics'
+        pname='Plunge'
+
         strikes = list()
         dips = list()
         strikesref = list()
         dipsref = list()
-
+        plunges=list()
+        kinematics=list()
+        rakes=list()
+        rakes_strikes=list()
+        rakes_dips=list()
+        rakes_pstrikes=list()
+        rakes_pdips=list()
         project = QgsProject.instance()
         proj_file_path=project.fileName()
         head_tail = os.path.split(proj_file_path)
 
-        gtCircles_flag_path = head_tail[0]+"/0. FIELD DATA/0. CURRENT MISSION/0. STOPS-SAMPLING-PHOTOGRAPHS-COMMENTS/gtCircles_flag.txt"
-
+        stereoConfigPath = head_tail[0]+"/0. FIELD DATA/0. CURRENT MISSION/0. STOPS-SAMPLING-PHOTOGRAPHS-COMMENTS/stereonet.json"
         
-        if(os.path.exists(gtCircles_flag_path)):
-            gtCircles=True
-        else:
-            gtCircles=False
+        stereoConfig={'showGtCircles':True,'showContours':True,'showKinematics':True}
+        
+
+        if(os.path.exists(stereoConfigPath)):
+            with open(stereoConfigPath,"r") as json_file:
+                stereoConfig=json.load(json_file)
         
         self.iface.layerTreeView().selectedLayers()
 
@@ -91,7 +178,7 @@ class Stereonet:
                 plungeExists = layer.fields().lookupField(pname)
                 srefExists = layer.fields().lookupField(srefname)
                 drefExists = layer.fields().lookupField(drefname)
-                
+
                 for feature in iter:
                     if strikeExists != -1 and dipExists != -1:
                         strikes.append(feature[sname])
@@ -109,6 +196,16 @@ class Stereonet:
                         if(not feature[srefname] is None and not feature[drefname] is None):
                             strikesref.append(feature[srefname])
                             dipsref.append(feature[drefname])
+                    
+                    if plungeExists != -1 and drefExists != -1:
+                        if(feature[pname]  and  feature[drefname]  and  feature[kname] ):
+                            rakes.append(self.plungedip2rake(feature[pname],feature[drefname],feature[kname]))
+                            rakes_strikes.append(feature[srefname])
+                            rakes_dips.append(feature[drefname])
+                            kinematics.append(feature[kname])
+                            rakes_pstrikes.append(feature[aname]+90)
+                            rakes_pdips.append(90-feature[pname])
+
 
             else:
                 continue
@@ -116,6 +213,7 @@ class Stereonet:
         dipsref = [i for i in dipsref if i != None]
         strikes = [i for i in strikes if i != None]
         dips = [i for i in dips if i != None]
+        plunges = [i for i in plunges if i != None]
         #print(strikes)
 
         if (len(strikes) != 0):
@@ -123,13 +221,17 @@ class Stereonet:
             ax.set_azimuth_ticks([0,30,60,90,120,150,180,210,240,270,300,330])
             ax.set_azimuth_ticklabels(['0\u00b0','30\u00b0','60\u00b0','90\u00b0','120\u00b0','150\u00b0','180\u00b0','210\u00b0','240\u00b0','270\u00b0','300\u00b0','330\u00b0'])
             ax.grid(kind='equal_area_stereonet')
-            ax.density_contour(strikes, dips, measurement='poles',cmap=cm.coolwarm,method='exponential_kamb',sigma=1.5,linewidths =0.5)
-            if(gtCircles and strikeExists != -1):
+            if(stereoConfig['showContours']):
+                ax.density_contour(strikes, dips, measurement='poles',cmap=cm.coolwarm,method='exponential_kamb',sigma=1.5,linewidths =0.5)
+            if(stereoConfig['showGtCircles'] and strikeExists != -1):
                 ax.plane(strikes, dips, 'k',linewidth=1)
             else:
                 ax.pole(strikes, dips, 'k.', markersize=5)
                 if(srefExists != -1 and drefExists != -1 ):
                     ax.plane(strikesref,dipsref,'k',linewidth=1)
+                if plungeExists != -1 and drefExists != -1 and stereoConfig['showKinematics']:
+                    self.waxi_fault_and_striae_plot(ax, rakes_pstrikes, rakes_pdips,rakes_strikes, rakes_dips, rakes,kinematics)
+
             ax.set_title(layer.name()+" [# "+str(len(iter))+"]")
             plt.show()
         else:
